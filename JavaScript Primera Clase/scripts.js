5e6d5c63398bf32889beb116c332a4a5bd6da5cb
Adán <adanxd97@gmail.com>
2022-10-03 16:08:15 -0300

    q

--- JavaScript Primera Clase/scripts.js
+++ JavaScript Primera Clase/scripts.js
@@ -1,1 +1,35 @@
-alert(parseInt());
+// var nombrePersona = "Adán";
+// let edad = 22;
+// prompt("edad");
+// document.writeln("Hola " + nombrePersona + " tenés " + edad + " años");
+
+
+// let caca = edad + nombrePersona;
+// console.log(caca);
+
+// var nombre;
+
+
+
+$('#botonSaludar').click(function(){
+    console.log("a");
+    let nombre = $("#nombreGente").val();
+
+    alert("Hola" + nombre);
+})
+
+// function saludar(){
+//     nombre = document.getElementById("nombre")
+//     alert("Hola" + nombre);
+// }
+
+
+// function saludar() {
+
+//     let nombreGente;
+
+//     nombreGente = document.getElementById("nombreGente").value;
+//     console.log(nombreGente);
+//     document.getElementById("saludo").innerHTML = "Hola " + nombreGente;
+//     document.getElementById("botonSaludar").style.display = "none";
+// }
